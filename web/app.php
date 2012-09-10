820de072568a4e77947c4cf477ac3532b8477125
Klaas Naaijkens <klaas.naaijkens@gmail.com>
2012-09-10 14:21:50 +0530

    apache change

--- web/app.php
+++ web/app.php
@@ -4,12 +4,15 @@ require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ApacheRequest;
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
-$request = Request::createFromGlobals();
+
+$request = ApacheRequest::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
+
