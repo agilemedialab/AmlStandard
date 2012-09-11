cce2128f5530c5753fd8fcd54228d0adce47ac0d
Klaas Naaijkens <klaas.naaijkens@gmail.com>
2012-09-11 12:07:44 +0530

    removed apachekernel change

--- web/app.php
+++ web/app.php
@@ -4,13 +4,13 @@ require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-use Symfony\Component\HttpFoundation\ApacheRequest;
+use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
-$request = ApacheRequest::createFromGlobals();
+$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
